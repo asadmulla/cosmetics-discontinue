30b7b897a893e678f38787a887b236d93a643246
asadmulla <81824451+asadmulla@users.noreply.github.com>
2025-02-21 11:14:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write("# Welcome to the Cosmetics Analysis Platform! 💄")
+st.image("Screenshot 2025-02-19 172208.png", caption="Empowering Safer Cosmetic Choices", use_container_width=True)
