d708261a0d35485c03d3f486cc5083cba424f26b
asadmulla <81824451+asadmulla@users.noreply.github.com>
2025-03-20 13:41:17 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.write("# Welcome to the Cosmetics Analysis Platform! 💄")
-st.image("Screenshot 2025-02-19 172208.png", caption="Empowering Safer Cosmetic Choices", use_container_width=True)
+st.image("image.png", caption="Empowering Safer Cosmetic Choices", use_container_width=True)
